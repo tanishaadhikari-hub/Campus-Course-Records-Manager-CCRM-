7b4641d263d510ddc7ff151752e63783f2585618
tanishaadhikari-hub <tanishaadhikarididi@gmail.com>
2026-09-18 19:16:16 +0530

    Create Max_Credit_Limit_Exceeded_exception.java

---
+++ src/edu/ccrm/exceptions/Max_Credit_Limit_Exceeded_exception.java
@@ -0,0 +1,16 @@
+package edu.ccrm.exceptions;
+
+public class Max_Credit_Limit_Exceeded_exception extends Exception
+{
+
+
+
+    public Max_Credit_Limit_Exceeded_exception(String message)
+
+
+    {
+
+        super(message)
+        ;
+    }
+}
